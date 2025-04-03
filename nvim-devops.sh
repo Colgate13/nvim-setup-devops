8133ba7e85e1fdd4d037ab6d663ddeea38968abd
Gabriel Barros Feitosa Sá <54006467+Colgate13@users.noreply.github.com>
2025-04-03 15:45:57 -0300

    Update nvim-devops.sh

--- nvim-devops.sh
+++ nvim-devops.sh
@@ -1,12 +1,12 @@
 sudo apt update && \
-sudo apt install -y curl software-properties-common && \
-sudo add-apt-repository ppa:neovim-ppa/stable -y && \
+sudo apt install -y curl git software-properties-common && \
+sudo add-apt-repository -y ppa:neovim-ppa/stable && \
 sudo apt update && \
 sudo apt install -y neovim && \
 mkdir -p ~/.config/nvim && \
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
-cat > ~/.config/nvim/init.vim << 'EOF'
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
+cat << 'EOF' > ~/.config/nvim/init.vim
 call plug#begin('~/.vim/plugged')
 
 Plug 'dracula/vim', { 'as': 'dracula' }
@@ -21,6 +21,7 @@ set tabstop=4
 set shiftwidth=4
 set expandtab
 set mouse=a
-colorscheme dracula
-EOF && \
-nvim +PlugInstall +qall
+EOF
+
+nvim +PlugInstall +qall && \
+echo 'colorscheme dracula' >> ~/.config/nvim/init.vim
